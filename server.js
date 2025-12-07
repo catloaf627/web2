40adb851a288fa066a42e673e1d3c3a8ba981153
Yuqing <wangyuqing.rachel@gmail.com>
2025-12-06 22:19:21 -0500

    Update port to support Render

--- server.js
+++ server.js
@@ -45,5 +45,7 @@ app.post("/process", async (req, res) => {
   res.render("result", { result });
 });
 
-const PORT = process.env.PORT || 3000;
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server running on port ${process.env.PORT || 3000}`);
+});
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
